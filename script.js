e58403629a48feca073da5a0fb1c3bb074d98e3d
Lucas Castelan Prado <castelanprado@gmail.com>
2017-03-03 23:53:12 -0300

    adding API's

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   position();
-  reverseGeocoding();
 });
 
 function position() {
@@ -13,23 +12,50 @@ function position() {
       var longitude = data.location.lng;
       $('#latitude').text(latitude);
       $('#longitude').text(longitude);
+      reverseGeocoding(latitude,longitude);
+      weather(latitude,longitude);
     },
     error: function () {
-      $('#erro').text('Not working');
+      $('#erro').text('Can\'t find coordinates');
     }
   });
 }
 
-function reverseGeocoding() {
+function reverseGeocoding(lat,lng) {
   $.ajax({
     type: "POST",
-    url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=-27.6092,-48.582&key=AIzaSyA3Y5ZVypTIkG_a-7sN9pt6PoPWeOh1ueM", /*url a fazer request*/
+    url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyA3Y5ZVypTIkG_a-7sN9pt6PoPWeOh1ueM`, /*url a fazer request*/
     dataType:"json", /*tipo de dado a receber*/
     success: function (data) {
-      $('#place').text(data.results.formatted_address);
+      //console.log(JSON.stringify(data));
+      let place = data.results[2].formatted_address;
+      $('#place').text(place);
     },
     error: function () {
-      $('#erro2').text('Not working2');
+      $('#erro2').text("Can\'t find city");
+    }
+  });
+}
+
+function weather(lat,lng) {
+  $.ajax({
+    type: "GET",
+    url: `https://simple-weather.p.mashape.com/weatherdata?lat=${lat}&lng=${lng}`,
+    dataType:"json",
+    success: function (data) {
+      var temp = data.query.results.channel.item.condition.temp;
+      var cond = data.query.results.channel.item.condition.text;
+      //var imag = data.query.results.channel.image;
+      $('#temp').text(temp);
+      $('#cond').text(cond);
+      //$('#theDiv').prepend('<img id="theImg" src="theImg.png" />');
+    },
+    error: function () {
+      $('#erro3').text("Can\'t find weather");
+    },
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-Mashape-Key", "Ey82Qs9cYemshOTkDjmdGHedDKT7p1lD10ljsntsa1dNilbP8V");
+      xhr.setRequestHeader("Accept", "application/json");
     }
   });
 }
