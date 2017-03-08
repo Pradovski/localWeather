a6248bdd42e2d364dc39be4fbc698f02b01c965f
Lucas Castelan Prado <castelanprado@gmail.com>
2017-03-08 19:25:29 -0300

    adding icon

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
+  var celsius = false;
   position();
-  var celsius = true;
   $('#button').on('click',function() {
     var change = document.getElementById("temp").textContent;
     var degrees = parseFloat(change);
     if (celsius) {
       degrees = toFahrenheit(degrees);
-      $('#button').text("F to C");
+      $('#unity').text("˚F");
+      $('#button').text("See in Celsius");
     } else {
       degrees = toCelsius(degrees);
-      $('#button').text("C to F");
+      $('#unity').text("˚C");
+      $('#button').text("See in Fahrenheit");
     }
-    celsius = !celsius;
     $('#temp').text(degrees);
+    celsius = !celsius;
   });
 });
 
@@ -22,7 +24,7 @@ function position() {
     url: "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDJFD_JVDG5KvB5nHfowcTLtQlX5qDsk8U", /*url a fazer request*/
     dataType:"json", /*tipo de dado a receber*/
     success: function (data) {
-      console.log(JSON.stringify(data));
+      //console.log(JSON.stringify(data));
       var latitude = data.location.lat;
       var longitude = data.location.lng;
       reverseGeocoding(latitude,longitude);
@@ -53,36 +55,70 @@ function reverseGeocoding(lat,lng) {
 function weather(lat,lng) {
   $.ajax({
     type: "GET",
-    url: `https://simple-weather.p.mashape.com/weatherdata?lat=${lat}&lng=${lng}`,
-    dataType:"json",
+    url: `https://api.darksky.net/forecast/1c6b339f3aa68d0ec000ade2a7f039ef/${lat},${lng}`,
+    dataType:"jsonp",
+    data: {
+      "units" : "us",
+      "exclude" : "minutely,hourly,daily,alerts,flags"
+    },
     success: function (data) {
-      var temp = data.query.results.channel.item.condition.temp;
-      var cond = data.query.results.channel.item.condition.text;
-      //var imag = data.query.results.channel.image;
+      var temp = data.currently.temperature;
+      var cond = data.currently.summary;
       $('#temp').text(temp);
       $('#cond').text(cond);
-      //$('#theDiv').prepend('<img id="theImg" src="theImg.png" />');
+      $("#icon").attr("src", getIcon(data.currently.icon));
     },
     error: function () {
       $('#temp').text("Couldn\'t find weather");
-    },
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader("X-Mashape-Key", "Ey82Qs9cYemshOTkDjmdGHedDKT7p1lD10ljsntsa1dNilbP8V");
-      xhr.setRequestHeader("Accept", "application/json");
     }
   });
 }
 
 function toFahrenheit(degrees) {
   degrees = degrees * (9/5) + 32;
-  degrees = degrees * 10;
+  degrees = degrees * 100;
   degrees = Math.round(degrees);
-  return degrees / 10;
+  return degrees / 100;
 }
 
 function toCelsius(degrees) {
   degrees = (degrees - 32) * (5/9);
-  degrees = degrees * 10;
+  degrees = degrees * 100;
   degrees = Math.round(degrees);
-  return degrees / 10;
+  return degrees / 100;
+}
+
+function getIcon(icon) {
+  var image;
+  switch (icon) {
+    case "clear-day":
+      image = "http://openweathermap.org/img/w/01d.png";
+      break;
+    case "clear-night":
+      image = "http://openweathermap.org/img/w/01n.png";
+      break;
+    case "rain":
+      image = "http://openweathermap.org/img/w/09d.png";
+      break;
+    case "snow":
+    case "sleet":
+      image = "http://openweathermap.org/img/w/13d.png";
+      break;
+    case "fog":
+    case "wind":
+      image = "http://openweathermap.org/img/w/50d.png";
+      break;
+    case "cloudy":
+      image = "http://openweathermap.org/img/w/04d.png";
+      break;
+    case "partly-cloudy-day":
+      image = "http://openweathermap.org/img/w/02d.png";
+      break;
+    case "partly-cloudy-night":
+      image = "http://openweathermap.org/img/w/02n.png";
+      break;
+    default:
+      image = "http://openweathermap.org/img/w/03d.png";
+  }
+  return image;
 }
